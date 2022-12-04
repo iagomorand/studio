a0e8c71b954395769cc05ac6c0bb5f67fdbcf224
Iago Morand <iagomorand@gmail.com>
2022-12-04 12:36:12 -0300

    atualizações de front

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-// let imagens = document.querySelectorAll('.small_img');
-// let modal = document.querySelector('.modal');
-// let modalImg = document.querySelector('#modal_img');
-// let btClose = document.querySelector('#bt_close');
-// let srcVal = "";
-
-// for(let i = 0; i<imagens.length; i++); {
-//     imagens[i].addEventListener('click',function(){
-//         alert(i);
-//     });
-// }
+document.querySelectorAll('')
